2de80285acb02e4f493372c3496855fde9e8db8f
Madeline Emily <93426401+madelinemily@users.noreply.github.com>
2023-09-12 17:03:03 +0700

    UPDATE

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">shoppee</a>
+      </header>
+      <main>
+        list of products
+      </main>
+    </div>
+  );
+}
+
+export default App;
