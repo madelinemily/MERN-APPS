30d48592ceb676fcdc7480ed55b54b648d97ae0d
Madeline Emily <93426401+madelinemily@users.noreply.github.com>
2023-09-12 17:26:52 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import logo from './logo.svg';
+import data from "./data";
 import './App.css';
 
 function App() {
@@ -8,7 +9,19 @@ function App() {
         <a href="/">shoppee</a>
       </header>
       <main>
-        list of products
+        <h1>Featured Products</h1>
+        {
+          data.products.map(product => (<div>
+            <img src={product.image} alt={product.name} />
+            <p>
+              {product.name}
+            </p>
+            <p>
+              {product.price}
+            </p>
+            </div>
+          )) 
+        }
       </main>
     </div>
   );
---
+++ src/data.js
@@ -0,0 +1,42 @@
+const data = {
+    products: [
+        {
+            name: 'Nike Slim',
+            slug: "nike-slim",
+            category: "Shirts",
+            image: "/images/p1.jpg",
+            price: 120,
+            countInStock: 10,
+            brand: "Nike",
+            rating: 4.5,
+            numReviews: 10,
+            description: "High quality",
+        },
+        {
+            name: 'Nike Slim',
+            slug: "nike-slim",
+            category: "Shirts",
+            image: "/images/p1.jpg",
+            price: 120,
+            countInStock: 10,
+            brand: "Nike",
+            rating: 4.5,
+            numReviews: 10,
+            description: "High quality",
+        },
+        {
+            name: 'Nike Slim',
+            slug: "nike-slim",
+            category: "Shirts",
+            image: "/images/p1.jpg",
+            price: 120,
+            countInStock: 10,
+            brand: "Nike",
+            rating: 4.5,
+            numReviews: 10,
+            description: "High quality",
+        },
+    ]
+}
+
+export default data;
